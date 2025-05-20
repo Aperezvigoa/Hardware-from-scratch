78b6bf9cc71e921e20d6d6392924b2f9d483f4e7
Alejandro Pérez-Vigo Albizu <108186387+Aperezvigoa@users.noreply.github.com>
2025-05-20 09:08:30 +0000

    Xor Implemented With Nand

--- Modulo 1 - Funciones Booleanas y Logica De Puertas/HDL CODE/Xor.vhdl
+++ Modulo 1 - Funciones Booleanas y Logica De Puertas/HDL CODE/Xor.vhdl
@@ -5,9 +5,20 @@ CHIP Xor {
     OUT out;
 
     PARTS:
-    Not(in=a, out=nota);
-	Not(in=b, out=notb);
-	And(a=a, b=notb, out=aAndNotb);
-	And(a=nota, b=b, out=notaAndb);
-	Or (a=aAndNotb, b=notaAndb, out=out);
+    // Not A & Not B
+    Nand(a= a, b= a, out= NotA);
+    Nand(a= b, b= b, out= NotB);
+
+    //  A And Not(B)
+    Nand(a= a, b= NotB, out= NandANotB);
+    Nand(a= NandANotB, b= NandANotB, out= aAndNotB);
+
+    // Not(A) And B
+    Nand(a= NotA, b= b, out= NandNotAb);
+    Nand(a= NandNotAb, b= NandNotAb, out= NotaAndB);
+
+    // (A And Not(B)) Or (Not(A) And B)
+    Nand(a= aAndNotB, b= aAndNotB, out= NandaAndNotB);
+    Nand(a= NotaAndB, b= NotaAndB, out= NandNotaAndB);
+    Nand(a= NandaAndNotB, b= NandNotaAndB, out= out);
 }
